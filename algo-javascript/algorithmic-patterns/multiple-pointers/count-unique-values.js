9e9b6fc0af16850d5ab63b602728521e1351fcfd
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-09-01 16:21:34 +0900

    고유값 세기 해결책 추가

--- algo-javascript/algorithmic-patterns/multiple-pointers/count-unique-values.js
+++ algo-javascript/algorithmic-patterns/multiple-pointers/count-unique-values.js
@@ -2,7 +2,7 @@
 // 배열에 음수가 있을 수 있지만 항상 정렬된다.
 
 // my solution 1
-function countUniqueValues2(values) {
+function countUniqueValues1(values) {
   if (!values.length) return 0;
 
   const countUniqueList = [];
@@ -21,7 +21,7 @@ function countUniqueValues2(values) {
 }
 
 // my solution 2 (힌트듣고 수정)
-function countUniqueValues(values) {
+function countUniqueValues2(values) {
   let result = 0;
 
   if (!values.length) return result;
@@ -35,9 +35,21 @@ function countUniqueValues(values) {
   return result;
 }
 
-// teacher solution
+// [권장] solution provided by the teacher - 추가 메모리를 사용하지 않고 중복 제거
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+  var i = 0;
+  for (var j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
 
 console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
 console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
 console.log(countUniqueValues([]));
 console.log(countUniqueValues([-2, -1, -1, 0, 1]));
+console.log(countUniqueValues([1, 2, 2, 5, 7, 7, 99]));
